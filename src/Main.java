7d695ad641638091ae69dd3f94382d3aac388bbf
Furkan Emir <furkanemirkaya@hotmail.com>
2024-01-06 12:58:52 +0300

    al sana da çay

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    private final String isim;
+
+    public Main() {
+        this.isim = null;
+    }
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
