d6a9af38b1e7024d7b271eae10df74b29fe39265
jojo <gorkembogacioglu@gmail.com>
2024-01-07 15:56:13 +0300

    jojo

--- src/DosyaIslemleri.java
+++ src/DosyaIslemleri.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public interface DosyaIslemleri {
     void dosyaOkuVeTxtEkranaYazdir(String dosyaAdi); // TXT dosyasındaki verileri okuyup TXT'yi olduğu gibi ekrana yazdıran metot
-    void dosyaOkuVeArrayListeEkle(); // TXT dosyasındaki verileri okuyup TXT'deki verileri ArrayList'e ekleyen metot
+    void dosyaOkuVeArrayListeEkle(String dosyaAdi); // TXT dosyasındaki verileri okuyup TXT'deki verileri ArrayList'e ekleyen metot
     void arrayListEkranaYazdir(); // Sadece ArrayList'i ekrana yazdıran metot
     void txtDosyasinaEkle(String dosyaAdi, String eklenecekVeri); // TXT dosyasına veriyi ekleyen metot
     String dosyaOkuVeReturnle(String dosyaAdi); // TXT dosyasındaki verileri satır satır okur ve değeri return eder metot.
